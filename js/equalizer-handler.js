c310365853e63af9b03f234aabf95a95ef5d8436
Ryan Stewart <ryanstewartalex@gmail.com>
2023-06-19 14:40:15 -0500

    Update

---
+++ js/equalizer-handler.js
@@ -0,0 +1,12 @@
+// let playing = false;
+//
+// const context = new AudioContext();
+// const equalizer = new Equalizer(context).appendTo(document.body);
+//
+// const osc = context.createOscillator();
+// osc.type = 'square';
+// osc.frequency.setValueAtTime(440, context.currentTime);
+// osc.connect(context.destination);
+// osc.start();
+//
+// equalizer.add(osc);
