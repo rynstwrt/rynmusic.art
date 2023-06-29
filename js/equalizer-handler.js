72edcc1f8267ce6e1c44304d83404b18347d3e17
Ryan Stewart <ryanstewartalex@gmail.com>
2023-06-29 16:09:00 -0500

    Remove Logging

--- js/equalizer-handler.js
+++ js/equalizer-handler.js
@@ -238,8 +238,6 @@ function animate()
     const average = fbcArray.reduce((a, b) => a + b) / fbcArray.length;
     logoContainer.style.transform = `scale(${1 + average / 1000})`;
 
-    console.log(audio.volume)
-
     const percent = audio.currentTime / audio.duration;
     slider.style.width = `${sliderContainer.clientWidth * percent}px`;
 }
