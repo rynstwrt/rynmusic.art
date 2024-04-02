6e7d5debd81c93ae5009405c163f44ae1d77dd71
Ryan Stewart <ryanstewartalex@gmail.com>
2024-04-02 15:10:15 -0500

    Update ui-handler.js

--- js/ui-handler.js
+++ js/ui-handler.js
@@ -136,29 +136,29 @@ window.addEventListener("load", () =>
 
     document.getElementById("audio-player").onplay = () =>
     {
-        context = new AudioContext();
-        analyser = context.createAnalyser();
-
-        canvasContext = canvas.getContext("2d");
-        const source = context.createMediaElementSource(audio);
-
-        source.connect(analyser);
-        analyser.connect(context.destination);
-
-        console.log("set up");
-        // if (context === undefined)
-        // {
-        //     context = new AudioContext();
-        //     analyser = context.createAnalyser();
+        // context = new AudioContext();
+        // analyser = context.createAnalyser();
         //
-        //     canvasContext = canvas.getContext("2d");
-        //     const source = context.createMediaElementSource(audio);
+        // canvasContext = canvas.getContext("2d");
+        // const source = context.createMediaElementSource(audio);
         //
-        //     source.connect(analyser);
-        //     analyser.connect(context.destination);
+        // source.connect(analyser);
+        // analyser.connect(context.destination);
         //
-        //     console.log("set up");
-        // }
+        // console.log("set up");
+        if (context === undefined)
+        {
+            context = new AudioContext();
+            analyser = context.createAnalyser();
+
+            canvasContext = canvas.getContext("2d");
+            const source = context.createMediaElementSource(audio);
+
+            source.connect(analyser);
+            analyser.connect(context.destination);
+
+            console.log("set up");
+        }
 
         animate();
     }
@@ -189,6 +189,7 @@ function animate()
     }
 
     const average = fbcArray.reduce((a, b) => a + b) / fbcArray.length;
+    console.log(average);
     logoContainer.style.transform = `scale(${1 + average / 1000})`;
 
     const percent = audioManager.getAudioProgressPercent();
