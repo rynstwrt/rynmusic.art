d0b4cd96088349c1af9249e5581a4b25219948d6
Ryan Stewart <ryanstewartalex@gmail.com>
2024-04-02 04:18:43 -0500

    Add "Makes Us Stronger' And Make Browsing Sequential By Date

--- js/equalizer-handler.js
+++ js/equalizer-handler.js
@@ -4,14 +4,14 @@ const TITLE_PREFIX = "Ryn - ";
 const AUDIO_PATH = "assets/audio/";
 const AUDIOS = [
     {
-        file: "ItBeLikeThat.mp3",
-        title: "It Be Like That",
-        year: "2023"
+        file: "GameBitch.mp3",
+        title: "GameBitch™",
+        year: "2021"
     },
     {
-        file: "4AM.mp3",
-        title: "4 AM",
-        year: "2022"
+        file: "CeremonialFunk.mp3",
+        title: "Ceremonial Funk",
+        year: "2021"
     },
     {
         file: "ADeeperLove.mp3",
@@ -19,19 +19,29 @@ const AUDIOS = [
         year: "2022"
     },
     {
-        file: "Down.mp3",
-        title: "Down",
-        year: "2023"
+        file: "4AM.mp3",
+        title: "4 AM",
+        year: "2022"
     },
     {
-        file: "GameBitch.mp3",
-        title: "GameBitch™",
-        year: "2021"
+        file: "IDontKnowIfIWantToGoYet.mp3",
+        title: "I Don't Know If I Want To Go Yet",
+        year: "2022"
     },
     {
-        file: "Gutted.mp3",
-        title: "Gutted",
-        year: "2023"
+        file: "MaybeIt'llGetBetter.mp3",
+        title: "Maybe It'll Get Better",
+        year: "2022"
+    },
+    {
+        file: "HouseOfPianoHouse.mp3",
+        title: "House of Piano House",
+        year: "2022"
+    },
+    {
+        file: "PianoBeat.mp3",
+        title: "Piano Beat",
+        year: "2022"
     },
     {
         file: "It'llBeFine.mp3",
@@ -44,23 +54,28 @@ const AUDIOS = [
         year: "2022"
     },
     {
-        file: "MaybeIt'llGetBetter.mp3",
-        title: "Maybe It'll Get Better",
-        year: "2022"
+        file: "I'mGonnaDishYouRightNow.mp3",
+        title: "I'm Gonna Dish You Right Now!",
+        year: "2023"
     },
     {
-        file: "PianoBeat.mp3",
-        title: "Piano Beat",
-        year: "2022"
+        file: "ItBeLikeThat.mp3",
+        title: "It Be Like That",
+        year: "2023"
+    },
+    {
+        file: "Gutted.mp3",
+        title: "Gutted",
+        year: "2023"
+    },
+    {
+        file: "Down.mp3",
+        title: "Down",
+        year: "2023"
     },
     {
         file: "Transfiguration.wav",
         title: "Transfiguration",
-        year: "2023"
-    },
-    {
-        file: "I'mGonnaDishYouRightNow.mp3",
-        title: "I'm Gonna Dish You Right Now!",
         year: "2023"
     },
     {
@@ -83,6 +98,7 @@ const AUDIOS = [
         title: "Amalgam",
         year: "2023"
     },
+
     {
         file: "WhatIfYouFly.mp3",
         title: "What If You Fly?",
@@ -101,6 +117,11 @@ const AUDIOS = [
     {
         file: "ImPerfect.mp3",
         title: "I'm Perfect!",
+        year: "2024"
+    },
+    {
+        file: "MakesUsStronger.mp3",
+        title: "Makes Us Stronger",
         year: "2024"
     }
 ];
@@ -178,7 +199,7 @@ function skipBackwardsOrForward(isBackward)
 function prevOrNextSong(isPrev)
 {
     nowPlaying.textContent = "";
-    songIndex += isPrev ? -1 : 1;
+    songIndex += isPrev ? 1 : -1;
 
     if (songIndex < 0)
         songIndex = AUDIOS.length - 1;
@@ -224,7 +245,8 @@ sliderContainer.addEventListener("click", event =>
 
 window.addEventListener("load", () =>
 {
-    songIndex = Math.floor(Math.random() * AUDIOS.length);
+    // songIndex = Math.floor(Math.random() * AUDIOS.length);
+    songIndex = AUDIOS.length - 1;
     audio = new Audio(AUDIO_PATH + AUDIOS[songIndex].file);
     audio.id = "audio-player";
 
